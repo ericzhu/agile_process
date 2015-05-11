93965b61e4d3d05b5454bc19b4a646c01b315112
Eric Zhu <eric.zhu@infor.com>
2015-05-11 16:52:13 -0400

    implements comparable interface

--- TDD2_FootballTeam/src/main/java/com/tdd/demo1/football/FootballTeam.java
+++ TDD2_FootballTeam/src/main/java/com/tdd/demo1/football/FootballTeam.java
@@ -1,15 +1,28 @@
 package com.tdd.demo1.football;
 
-public class FootballTeam {
+public class FootballTeam implements Comparable<FootballTeam> {
 
 	private int gamesWon;
-	
+
 	public FootballTeam(int gamesWon) {
+		if (gamesWon < 0) {
+			throw new IllegalArgumentException("The gamesWon passed to constructor should be positive, but the actual is [" + gamesWon + "]");
+		}
 		this.gamesWon = gamesWon;
 	}
 
+	/**
+	 * The number of the games won by this team.
+	 * 
+	 * @return
+	 */
 	public int getGamesWon() {
 		return this.gamesWon;
 	}
 
+	@Override
+	public int compareTo(FootballTeam o) {
+		
+		return this.gamesWon - o.getGamesWon();
+	}
 }
--- TDD2_FootballTeam/src/test/java/com/tdd/demo1/football/FootballTeamTest.java
+++ TDD2_FootballTeam/src/test/java/com/tdd/demo1/football/FootballTeamTest.java
@@ -1,20 +1,75 @@
 package com.tdd.demo1.football;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+@RunWith(JUnitParamsRunner.class)
 public class FootballTeamTest {
 
 	private static final int TEN_TIMES_WON = 10;
+	private static final int TWO_TIMES_WON = 2;
+	private static final int ANY_NUMBER = 123;
+
+	@SuppressWarnings("unused")
+	private static final Object[] nbOfValidGamesWon() {
+		return new Object[][] { { 1 }, { 10 }, { 100 } };
+	}
+
+	@SuppressWarnings("unused")
+	private static final Object[] invalidNbOfValidGamesWon() {
+		return new Object[][] { { -1 }, { -10 }, { -100 } };
+	}
 
 	@Test
-	public void test_constructor_setGamesWonInConstructor() {
-		FootballTeam target = new FootballTeam(TEN_TIMES_WON);
+	@Parameters(method = "nbOfValidGamesWon")
+	public void test_constructor_constructorShouldSetGamesWon(int nbOfGamesWon) {
+		FootballTeam target = new FootballTeam(nbOfGamesWon);
 
-		assertEquals("Games won passed in constructor is " + TEN_TIMES_WON + ", but getGameWon() returns " + //
-				target.getGamesWon(), TEN_TIMES_WON, target.getGamesWon());
+		assertEquals("Games won passed in constructor is " + nbOfGamesWon + ", but getGameWon() returns " + //
+				target.getGamesWon(), nbOfGamesWon, target.getGamesWon());
 
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	@Parameters(method = "invalidNbOfValidGamesWon")
+	public void test_constructor_GamesWonShouldNotBeNegative(int gamesWon) {
+		new FootballTeam(gamesWon);
+	}
+
+	@Test
+	public void test_instanceOfFootballTeamShouldBeComparable() {
+		assertTrue("The FootballTeam class should implement Comparable", new FootballTeam(ANY_NUMBER) instanceof Comparable);
+	}
+
+	@Test
+	public void test_compareTo_moreGamesWonShouldBeGreater() {
+		FootballTeam tenTimesWon = new FootballTeam(TEN_TIMES_WON);
+		FootballTeam twoTimesWon = new FootballTeam(TWO_TIMES_WON);
+
+		assertTrue("Team wan " + TEN_TIMES_WON + " times should be greater than team wan " + TWO_TIMES_WON + " times", tenTimesWon.compareTo(twoTimesWon) > 0);
+
+	}
+
+	@Test
+	public void test_compareTo_lessGamesWonShouldBeSmaller() {
+		FootballTeam twoTimesWon = new FootballTeam(TWO_TIMES_WON);
+		FootballTeam tenTimesWon = new FootballTeam(TEN_TIMES_WON);
+
+		assertTrue("Team wan " + TWO_TIMES_WON + " times should be smaller than team wan " + TEN_TIMES_WON + " times", twoTimesWon.compareTo(tenTimesWon) < 0);
+
+	}
+
+	@Test
+	public void test_compareTo_sameNbOfGamesWonShouldBeEqual() {
+		FootballTeam team1 = new FootballTeam(ANY_NUMBER);
+		FootballTeam team2 = new FootballTeam(ANY_NUMBER);
+
+		assertTrue("Teams with the same number of games won should be equal", team1.compareTo(team2) == 0);
 	}
 
 }
